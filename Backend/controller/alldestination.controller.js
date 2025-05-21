7efec178a5662fdb33a5d22ebb99220395312baa
SaloniSharma <salonisharma16346@gmail.com>
2025-05-21 22:51:54 +0530

    Update alldestination.controller.js

--- Backend/controller/alldestination.controller.js
+++ Backend/controller/alldestination.controller.js
@@ -3,7 +3,7 @@ import Destination from "../models/alldestination.model.js";
 export const getAllDestinations = async (req, res) => {
   try {
     const destinations = await Destination.find();
-    res.status(200).json({data:destinations});
+   res.status(200).json(destinations);
   } catch (error) {
     res.status(500).json({ message: "Error fetching destinations", error:error.message });
   }

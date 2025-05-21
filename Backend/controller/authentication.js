24d8082633852b977d94f1abd6050fef3cbb9180
SaloniSharma <salonisharma16346@gmail.com>
2025-05-21 23:43:56 +0530

    Update authentication.js

--- Backend/controller/authentication.js
+++ Backend/controller/authentication.js
@@ -36,10 +36,12 @@ export const loginuser = async (req, res) => {
   const { number, password } = req.body;
 
   try {
-    const user = await User.findOne({ number: number, password: password });
+    const user = await User.findOne({ number }); 
+
     if (!user) {
-      return res.status(401).json({ message: "Invalid credentials" });
+      return res.status(401).json({ message: "Invalid number or password" });
     }
+
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
@@ -49,10 +51,14 @@ export const loginuser = async (req, res) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
+
     res
       .status(200)
-      .json({ message: "Login successful", data: user, token: token });
+      .json({ message: "Login successful", data: user, token });
   } catch (error) {
-    res.status(500).json({ message: "Error logging in", error: error.message });
+    res
+      .status(500)
+      .json({ message: "Error logging in", error: error.message });
   }
 };
+
